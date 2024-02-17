145c24162e0c9339f858f07d866c9a7a844f90af
Mochammad Enrique Lazuardi Ramadany <e41212006@student.polije.ac.id>
2024-02-17 21:34:18 +0700

    Updating mini version

--- resources/views/admin/template/structure.blade.php
+++ resources/views/admin/template/structure.blade.php
@@ -97,7 +97,7 @@
                 </g>
               </svg>
             </div>
-            <span class="nav-link-text ms-1">Anggota Dewan</span>
+            <span class="nav-link-text ms-1">Struktur Anggota</span>
           </a>
         </li>
         <li class="nav-item">
